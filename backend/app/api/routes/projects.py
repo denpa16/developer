bf76b8a5a686d6794029d7d309d92752eee0e09c
d.pavlov <d.pavlov@idaproject.com>
2024-08-30 21:48:16 +0300

    feat(api): add simple annotations

--- backend/app/api/routes/projects.py
+++ backend/app/api/routes/projects.py
@@ -1,8 +1,8 @@
 from fastapi import APIRouter
-from sqlalchemy import delete, select
+from sqlalchemy import delete, func, select
 from sqlalchemy.ext.asyncio import AsyncSession
 
-from app.data.models import Project
+from app.data.models import Building, Project
 from app.data.sqlalchemy_session import async_session
 
 router = APIRouter(prefix="/projects", tags=["Projects"])
@@ -11,8 +11,31 @@ router = APIRouter(prefix="/projects", tags=["Projects"])
 @router.get("/")
 async def get_list(session: AsyncSession = async_session):
     """Список проектов."""
-    result = await session.execute(select(Project))
-    return result.scalars().all()
+    subquery_building_count = (
+        select(
+            Building.project_id,
+            func.count(Building.id).label("building_count"),
+        )
+        .group_by(Building.project_id)
+        .subquery()
+    )
+    result = await session.execute(
+        select(
+            Project.id,
+            Project.name,
+            Project.alias,
+            func.coalesce(subquery_building_count.c.building_count, 0).label("building_count"),
+        ).outerjoin(subquery_building_count, subquery_building_count.c.project_id == Project.id),
+    )
+    return [
+        {
+            "id": str(proj.id),
+            "building_count": proj.building_count,
+            "name": proj.name,
+            "alias": proj.alias,
+        }
+        for proj in result
+    ]
 
 
 @router.get("/bulk_create")
