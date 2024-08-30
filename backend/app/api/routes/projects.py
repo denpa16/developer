451a0686b53620cdcbd65a2d8bdaa8920775b24f
d.pavlov <d.pavlov@idaproject.com>
2024-08-30 21:53:01 +0300

    feat(api): add simple annotations

--- backend/app/api/routes/projects.py
+++ backend/app/api/routes/projects.py
@@ -2,7 +2,7 @@ from fastapi import APIRouter
 from sqlalchemy import delete, func, select
 from sqlalchemy.ext.asyncio import AsyncSession
 
-from app.data.models import Building, Project
+from app.data.models import Building, Project, Section
 from app.data.sqlalchemy_session import async_session
 
 router = APIRouter(prefix="/projects", tags=["Projects"])
@@ -19,20 +19,34 @@ async def get_list(session: AsyncSession = async_session):
         .group_by(Building.project_id)
         .subquery()
     )
+    subquery_section_count = (
+        select(
+            Project.id.label("project_id"),
+            func.count(Section.id).label("section_count"),
+        )
+        .join(Building, Building.project_id == Project.id)
+        .join(Section, Section.building_id == Building.id)
+        .group_by(Project.id)
+        .subquery()
+    )
     result = await session.execute(
         select(
             Project.id,
             Project.name,
             Project.alias,
             func.coalesce(subquery_building_count.c.building_count, 0).label("building_count"),
-        ).outerjoin(subquery_building_count, subquery_building_count.c.project_id == Project.id),
+            func.coalesce(subquery_section_count.c.section_count, 0).label("section_count"),
+        )
+        .outerjoin(subquery_building_count, subquery_building_count.c.project_id == Project.id)
+        .outerjoin(subquery_section_count, subquery_section_count.c.project_id == Project.id),
     )
     return [
         {
             "id": str(proj.id),
-            "building_count": proj.building_count,
             "name": proj.name,
             "alias": proj.alias,
+            "building_count": proj.building_count,
+            "section_count": proj.section_count,
         }
         for proj in result
     ]
