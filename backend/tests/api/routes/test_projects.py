08bc7d034efa2eb747a450765cd245034dd449cb
d.pavlov <d.pavlov@idaproject.com>
2024-10-09 21:52:17 +0300

    feat: привел тесты к нормальному виду

--- backend/tests/api/routes/test_projects.py
+++ backend/tests/api/routes/test_projects.py
@@ -8,9 +8,11 @@ from tests.factories.projects import ProjectFactory
 @pytest.mark.asyncio
 class TestProjects:
     async def test_list(self, api_client: AsyncClient, sqlalchemy_assert_max_num_queries):
-        await ProjectFactory()
+        projects_count = 5
+        projects = [await ProjectFactory() for _ in range(projects_count)]
         with sqlalchemy_assert_max_num_queries(1):
             response = await api_client.get("/api/projects/")
         assert response.status_code == 200
         res_json = response.json()
-        assert res_json["count"] == 1
+        assert res_json["count"] == projects_count
+        assert res_json["results"][0]["id"] == str(projects[0].id)
--- backend/tests/conftest.py
+++ backend/tests/conftest.py
@@ -9,38 +9,16 @@ from sqlalchemy import create_engine, event
 from sqlalchemy.ext.asyncio import AsyncSession
 from app.data.sqlalchemy_session import AsyncSessionBuilder
 from sqlalchemy_utils import create_database, drop_database
-from async_factory_boy.factory.sqlalchemy import AsyncSQLAlchemyFactory
 
 from app.config import settings
 from app.data.models import Base, Project
 
-
-def t_test_session(t_dsn) -> AsyncSession:
-    async_session_builder = AsyncSessionBuilder(database_url=t_dsn, echo=settings.database.echo)
-    return async_session_builder()
 
 @pytest.fixture()
 def test_session(test_postgres_dsn) -> AsyncSession:
     test_db_dsn = test_postgres_dsn(scheme=settings.database.scheme)
     async_session_builder = AsyncSessionBuilder(database_url=test_db_dsn, echo=settings.database.echo)
     yield async_session_builder()
-
-
-def t_test_postgres_dsn() -> Callable:
-    def build_dsn(**kwargs) -> str:
-        # db_name = "_".join((s.lower() for s in settings.api.title.split(" ")))
-        db_name = "FastApi"
-        test_db_dsn = PostgresDsn.build(
-            scheme=kwargs.get("scheme") or settings.database.scheme,
-            username=kwargs.get("user") or settings.database.user,
-            password=kwargs.get("password") or settings.database.password,
-            host=kwargs.get("host") or settings.database.host,
-            port=kwargs.get("port") or settings.database.port,
-            path=f"/pytest_{db_name}",
-        )
-        return test_db_dsn.unicode_string()
-
-    return build_dsn
 
 
 @pytest.fixture(scope="session")
@@ -148,13 +126,27 @@ def sqlalchemy_assert_num_queries(test_session):
         event.remove(t_session.sync_session.bind.engine, "before_cursor_execute", before_cursor_execute)
     return check_count_database_queries
 
-@pytest.fixture()
-async def create_projects(test_session):
-    class ProjectFactory(AsyncSQLAlchemyFactory):
-        class Meta:
-            model = Project
-            sqlalchemy_session = test_session()
+def factory_test_postgres_dsn() -> Callable:
+    def build_dsn(**kwargs) -> str:
+        # db_name = "_".join((s.lower() for s in settings.api.title.split(" ")))
+        db_name = "FastApi"
+        test_db_dsn = PostgresDsn.build(
+            scheme=kwargs.get("scheme") or settings.database.scheme,
+            username=kwargs.get("user") or settings.database.user,
+            password=kwargs.get("password") or settings.database.password,
+            host=kwargs.get("host") or settings.database.host,
+            port=kwargs.get("port") or settings.database.port,
+            path=f"/pytest_{db_name}",
+        )
+        return test_db_dsn.unicode_string()
+    return build_dsn
 
-        name = "w2e2e2dwpdwpedwnd"
 
-    await ProjectFactory.create()
+def factory_test_session(t_dsn) -> AsyncSession:
+    async_session_builder = AsyncSessionBuilder(database_url=t_dsn, echo=settings.database.echo)
+    return async_session_builder()
+
+def get_factory_session():
+    t_dsn = factory_test_postgres_dsn()
+    t_session = factory_test_session(t_dsn())
+    return t_session()
